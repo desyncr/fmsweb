902635d25981e8195dd5703e508122cb34e086c4
Dario Cavuotti <dario.cavuotti@tiendanube.com>
2020-07-18 22:10:23 -0300

    Add springboot structure

---
+++ src/test/java/com/github/desyncr/fmsweb/FmswebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.desyncr.fmsweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmswebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
